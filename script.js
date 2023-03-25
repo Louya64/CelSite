f27cbd405eeee41d823be1991d108bcfe79f9c61
Louya64 <cixie@live.fr>
2023-03-25 10:52:01 +0100

    test responsive tablet

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const dropdownTrigger = document.getElementById("dropdownTrigger");
-dropdownTrigger.addEventListener("click", () => {
-	dropdownTrigger.classList.toggle("active");
-});
+const openDropdown = () => {
+	dropdownTrigger.classList.add("active");
+	dropdownTrigger.removeEventListener("click", openDropdown);
+	setTimeout(() => {
+		document.addEventListener("click", closeDropdown, { once: true });
+	}, 200);
+};
+const closeDropdown = () => {
+	dropdownTrigger.classList.remove("active");
+	document.removeEventListener("click", closeDropdown);
+	setTimeout(() => {
+		dropdownTrigger.addEventListener("click", openDropdown);
+	}, 200);
+};
+
+dropdownTrigger.addEventListener("click", openDropdown);
 
 const iconBurger = document.getElementById("iconBurger");
 const nav = document.getElementById("nav");
